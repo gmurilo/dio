38d453710e8e6194e5aad6650d5b605b720ca28e
Giovani Correa <gmurilo@gmail.com>
2022-10-11 20:07:10 -0300

    primeira fase do layout do desafio do pokemon, porem, para o desafio, creio que eu possa considerar executado

---
+++ formacao-javascript-developer/construindo-uma-pokedex-com-javascript/assets/js/main.js
@@ -0,0 +1,40 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon border-${pokemon.type}">
+          <span class="number color-${pokemon.type}">#${pokemon.number}</span>
+          <img src="${pokemon.photo}"
+               alt="${pokemon.name}">
+          <span class="name bg-${pokemon.type}">${pokemon.name}</span>            
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+       const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
